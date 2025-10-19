e8faf9b9777f4d761b8875bac73c678b762f28be
RealBJr <juniorboni003@gmail.com>
2025-10-19 17:56:26 -0400

    Added backend call to map config loading

--- hexapedal_frontend/src/app/services/utils/loadMapConfig.ts
+++ hexapedal_frontend/src/app/services/utils/loadMapConfig.ts
@@ -1,12 +1,30 @@
 import { LatLngTuple } from 'leaflet';
+import { API_BASE_URL } from './constants';
 
-export default function loadMapConfig() {
-    return {
-        center: [40, 20] as LatLngTuple,
-        zoom: 5,
-        style: {
-            height: '500px',
-            width: '100%',
-        } as React.CSSProperties,
-    };
+export const fallBackMapConfig = {
+    center: [40, 20] as LatLngTuple,
+    zoom: 5,
+    style: {
+        height: '100%',
+        width: '100%',
+    } as React.CSSProperties,
+};
+
+export default async function loadMapConfig() {
+    try {
+        const response = await fetch(`${API_BASE_URL}/api/map-config`);
+        const mapConfig = await response.json();
+
+        return {
+            center: mapConfig.center as LatLngTuple,
+            zoom: mapConfig.zoom,
+            style: {
+                height: '100%',
+                width: '100%',
+            } as React.CSSProperties,
+        };
+    } catch (error) {
+        console.error('Error loading map config:', error);
+        return fallBackMapConfig;
+    }
 }
--- hexapedal_frontend/src/hooks/useMapConfig.ts
+++ hexapedal_frontend/src/hooks/useMapConfig.ts
@@ -1,7 +1,18 @@
-import loadMapConfig from '@/app/services/utils/loadMapConfig';
-import { useState } from 'react';
+import loadMapConfig, { fallBackMapConfig } from '@/app/services/utils/loadMapConfig';
+import { useEffect, useState } from 'react';
 
 export default function useMapConfig() {
-    const [mapConfig, setMapConfig] = useState(loadMapConfig());
+    const [mapConfig, setMapConfig] = useState(fallBackMapConfig);
+
+    // Asynchronously load the config
+    useEffect(() => {
+        async function fetchConfig() {
+            const config = await loadMapConfig();
+            setMapConfig(config);
+        }
+
+        fetchConfig();
+    }, [])
+
     return [mapConfig, setMapConfig];
 }
