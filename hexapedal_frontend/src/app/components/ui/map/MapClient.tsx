414ec52cbde5f80460ab4a98f2445face2db68f9
RealBJr <juniorboni003@gmail.com>
2025-10-19 16:11:27 -0400

    Added useMapConfig hook

--- hexapedal_frontend/src/app/components/ui/map/MapClient.tsx
+++ hexapedal_frontend/src/app/components/ui/map/MapClient.tsx
@@ -2,25 +2,16 @@
 
 import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import useMapConfig from '@/hooks/useMapConfig';
 
 export default function MapClient() {
+    const [mapConfig, setMapConfig] = useMapConfig();
     return (
-        <MapContainer center={[40, 20]} zoom={5} style={{ height: '500px', width: '100%' }}>
+        <MapContainer {...mapConfig}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <CircleMarker
-                center={[40.609787846393196, 20.7890265133657]}
-                radius={10}
-                color="transparent"
-                fillColor="green"
-                fillOpacity={0.5}
-            >
-                <Popup>
-                    <h2>Hello World</h2>
-                </Popup>
-            </CircleMarker>
         </MapContainer>
     );
 }
