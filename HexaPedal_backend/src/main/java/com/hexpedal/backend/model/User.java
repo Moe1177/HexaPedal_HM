2e5bd6ef4250d9770faa3d150d9997854f150651
TristanGirardi <t.girardi13@gmail.com>
2025-10-21 17:53:36 -0400

    Fixed Authenthication Issue That Prevented Accessing User Info

--- HexaPedal_backend/src/main/java/com/hexpedal/backend/model/User.java
+++ HexaPedal_backend/src/main/java/com/hexpedal/backend/model/User.java
@@ -1,20 +1,22 @@
 package com.hexpedal.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "role")
 public class User implements UserDetails {
@@ -25,46 +27,61 @@ public class User implements UserDetails {
     private String fullName;
     @Column(nullable = false)
     private String address;
-    @Column(unique=true,nullable = false)
+    @Column(unique = true, nullable = false)
     private String username;
-    @Column(unique=true,nullable = false)
+    @Column(unique = true, nullable = false)
     private String email;
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
     private boolean enabled;
+    @JsonIgnore
     @Column(name = "verification_code")
     private String verificationCode;
-    @Column(name="verification_expiration")
+    @JsonIgnore
+    @Column(name = "verification_expiration")
     private LocalDateTime verificationCodeExpiresAt;
 
     public User(String fullName, String address, String username, String email, String password) {
         this.fullName = fullName;
         this.address = address;
-//        this.role = role;
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    public User() {
-    }
-
     @Override
+    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
     }
 
     @Override
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getUsername() {
+        return email; // Using email as username for authentication
+    }
+
+    @Override
+    @JsonIgnore
     public boolean isAccountNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonLocked() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isCredentialsNonExpired() {
         return true;
     }
@@ -73,7 +90,4 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return enabled;
     }
-
-
 }
-
