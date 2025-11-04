ee44c45055393d9b73267d3cd5e2718f7b513758
RealBJr <juniorboni003@gmail.com>
2025-11-04 10:10:33 -0500

    Code cleaning - removed MapEntities Provider from MapView

--- hexapedal_frontend/src/app/components/views/MapView.tsx
+++ hexapedal_frontend/src/app/components/views/MapView.tsx
@@ -1,11 +1,8 @@
 import "leaflet/dist/leaflet.css";
 import MapServer from "../ui/map/MapServer";
-import { MapEntitiesProvider } from "@/app/providers/MapEntitiesProvider";
 
 export default function MapView() {
     return (
-        <MapEntitiesProvider>
-            <MapServer />
-        </MapEntitiesProvider>
+        <MapServer />
     );
 }
